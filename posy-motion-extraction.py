170504949483a536e0732c9eb3d79fe02fea02da
Kees Jan Koster <kjkoster@kjkoster.org>
2023-12-30 00:49:46 +0100

    Add time shift slider.

--- posy-motion-extraction.py
+++ posy-motion-extraction.py
@@ -3,11 +3,10 @@
 # A script that perform's Posy's motion extraction on a sample video clip. We
 # use Python and OpenCV.
 #
-# https://www.youtube.com/watch?v=NSS6yAMZF78
-#
 
 import cv2
 import numpy as np
+from os import path
 from sys import argv
 
 HORIZONTAL=1
@@ -16,33 +15,66 @@ if len(argv) != 2:
     print(f"usage: {argv[0]} <video file>")
     exit(1)
 
-FRAME_SHIFT=10
+
+# Define the time/frame shifting buffer. The amount of shift is tracked as
+# `frame_shift` and we keep the frames in the `delay_queue` until they have been
+# shifted enough to be processed.
+#
+# The value of `frame_shift` and thus the capacity of the delay queue gets set
+# dynamically from the trackbar on the main window, using the
+# `set_frame_shift()` callback function.
+#
+# We shift a minimum of 2 frames, otherwise the output is just a gray box.
+
+frame_shift=20
 delay_queue = []
+
+def set_frame_shift(new_frame_shift):
+    global frame_shift
+    if new_frame_shift < 2:
+        cv2.setTrackbarPos("delay (frames)", MAIN_WINDOW, 2)
+    else:
+        frame_shift = new_frame_shift
+
+
+# Name the main window so the user knows what they are looking at and the code
+# has a reference for the trackbar.
+
+MAIN_WINDOW=path.basename(argv[1])
+cv2.namedWindow(MAIN_WINDOW)
+cv2.createTrackbar("delay (frames)", MAIN_WINDOW, frame_shift, 100, set_frame_shift)
+
+
+# Open the video that was specified as command line argument, then process each
+# frame in turn.
 
 cap = cv2.VideoCapture(argv[1])
 while(cap.isOpened()):
     # first, just read the current frame
     have_frame, current_frame = cap.read()
     if not have_frame:
-        break
+        break # video done, release() and destroy windows
 
 
-    # Hold on to the oldest frame in the delat queue and add the current frame
-    # to the delay queue, moving everything down the queue one step. Then check
-    # if we reached our delay buffer's capacity yet. When we start, this buffer
-    # is empty and for the rest of the code to work we need it to be filled to
-    # capacity.
+    # We append the current frame to the end of the delay queue, then take the
+    # oldest frame from the queue for use as the delayed frame, below. This
+    # gives us the time shift that the algorithm asks for.
+    #
+    # A complicating factor is that we allow the user to set the time shift from
+    # the trackbar above the video. Since that can change at any time, we have
+    # to allow the buffer to expand and contract based on that setting. We solve
+    # this by clipping the buffer to the capacity if it is longer than what the
+    # user specified. This will handle reductions well.
 
     delay_queue.append(current_frame)
-    if len(delay_queue) < FRAME_SHIFT:
-        continue
-
+    delay_queue = delay_queue[-frame_shift:]
+    delayed_frame = delay_queue[0]
 
     # If we get here, we have the current frame and a suitably delayed frame.
     # For easy debugging, show these side-by-side.
 
-    delayed_frame = delay_queue.pop(0)
     combined_frames = np.concatenate((current_frame, delayed_frame), axis=HORIZONTAL) 
+
 
     # The delayed image needs some processing, though. The first operation is to
     # invert it.
@@ -50,11 +82,17 @@ while(cap.isOpened()):
     inverted_frame = cv2.bitwise_not(delayed_frame)
     combined_frames = np.concatenate((combined_frames, inverted_frame), axis=HORIZONTAL) 
 
+
+    # Then we blend the inverted and original frames, each at 50% alpha.
+
     blended_frame = cv2.addWeighted(current_frame, 0.5, inverted_frame, 0.5, 0)
     combined_frames = np.concatenate((combined_frames, blended_frame), axis=HORIZONTAL) 
 
+
+    # We can now render the frames.
+
     cv2.imshow('debugging', combined_frames)
-    cv2.imshow('frame', blended_frame)
+    cv2.imshow(MAIN_WINDOW, blended_frame)
 
     # & 0xFF is required for a 64-bit system
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- readme.md
+++ readme.md
@@ -1,30 +1,46 @@
 # Motion Extraction as per Posy
 
-This is a quick implementation of video motion extraction, using the method
-proposed by [Posy](https://www.youtube.com/@PosyMusic) in his video on Motion
-Extraction. (Yes, that splotchy image below is a video link, click it to watch
-Posy's inspiring video).
-
-[![Motion Extraction](https://img.youtube.com/vi/NSS6yAMZF78/hqdefault.jpg)](https://www.youtube.com/embed/NSS6yAMZF78)
-
-It was actually [Steve Mould](https://www.youtube.com/@SteveMould) who got me
-interested in video motion amplification. but I never got round to anything like
-experimental implementation. The math behind
-[Eulerian Video Magnification](https://people.csail.mit.edu/mrub/evm/) was
-always a bit daunting. When I ran into Posy's video, making this proof of
-concept was quick and easy.
-
-[![Reveal Invisible Motion With This Clever Video Trick](https://img.youtube.com/vi/rEoc0YoALt0/hqdefault.jpg)](https://www.youtube.com/embed/rEoc0YoALt0)
-
-Just to be clear: the code in this repository does _not_ implement Eulerian,
-only the simplified version that Posy proposes.
-
-Hope this helps.
+This program helps experiment with
+[Posy's motion extraction method](https://www.youtube.com/watch?v=NSS6yAMZF78).
+Posy proposed a simple algorithm to extract motion from a video. He shows off
+some pretty trippy results. If you would like to try this for yourself and don't
+have all the video processing programs that Posy uses, here is a version that
+you can run in Python.
 
 ## Running
+
+The code runs on Python 3, using Numy and OpenCV as dependencies. You will need
+a video that you want to experiment with.
 
 ```sh
 $ pip3 install python-opencv numpy
 $ python3 posy-motion-extraction.py some-video.mp4
 ```
+
+Substitute `some-video.mp4` with the file name of the video you would like to
+work on.
+
+The slider along the top of the main window allows you to control the time shift
+(in frames) of the motion extraction.
+
+## Background
+
+This is a quick implementation of video motion extraction, using the method
+proposed by [Posy](https://www.youtube.com/@PosyMusic) in his video on Motion
+Extraction.
+
+[![Motion Extraction](images/youtube-preview.png)](https://www.youtube.com/watch?v=NSS6yAMZF78)
+
+It was actually [Steve Mould](https://www.youtube.com/@SteveMould) who got me
+interested in
+[video motion amplification](https://www.youtube.com/watch?v=rEoc0YoALt0), but I
+never got round to anything like experimental implementation. The math behind
+[Eulerian Video Magnification](https://people.csail.mit.edu/mrub/evm/) was
+always a bit daunting. When I ran into Posy's video, making this proof of
+concept was quick and easy.
+
+Just to be clear: the code in this repository does _not_ implement Eulerian,
+only the simplified version that Posy proposes.
+
+Hope this helps.
 
